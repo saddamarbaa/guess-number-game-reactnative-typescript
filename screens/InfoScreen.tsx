340fd5da6c1a7ca738ba7f62e7cc59e12e20114b
saddamarbaa <saddamarbaas@gamil.com>
2022-10-04 08:27:37 +0700

    chore: migrate to - typeScript

--- screens/InfoScreen.tsx
+++ screens/InfoScreen.tsx
@@ -3,14 +3,18 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
-export default function TabTwoScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
-    </View>
-  );
+export default function TabInfoScreen() {
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>Tab Two</Text>
+			<View
+				style={styles.separator}
+				lightColor="#eee"
+				darkColor="rgba(255,255,255,0.1)"
+			/>
+			<EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
